7f9585bb7cd6a3228b2c81bb06f9d05240916647
Kike0013 <kique0013@hotmail.com>
2022-10-17 14:42:25 -0400

    Adding editing mode

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const flashcards = document.getElementsByClassName("flashcards")[0];
 const createBox = document.getElementsByClassName("create-box")[0];
 const question = document.getElementById("question")
 const answer = document.getElementById("answer")
+let nodeBtn = flashcards.childNodes;
 let contentArray = localStorage.getItem('fcards') ? JSON.parse(localStorage.getItem('fcards')) : [];
+let editMode = false;
 
 const delFlashcards = () => {
     localStorage.clear();
@@ -16,6 +18,15 @@ function divMaker(text) {
     let h2_question = document.createElement("h2");
     let h2_answer = document.createElement("h2");
 
+    //Edit card button code
+    let editBtn = document.createElement("button");
+    editBtn.innerHTML = 'Edit';
+    editBtn.id = 'edit-card'
+    editBtn.setAttribute('style', "display: none; position: absolute; padding: 5px; margin: 2px")
+    div.appendChild(editBtn)
+
+    //^^^^^^^^^^^^^^^^^^^
+
     div.className = 'flashcard';
 
     h2_question.setAttribute('style', "border-top: 1px solid red; padding: 15px; margin-top: 30px;")
@@ -23,20 +34,24 @@ function divMaker(text) {
 
     h2_answer.setAttribute('style', "text-align: center; display: none; color: red;")
     h2_answer.innerHTML = text.my_answer;
+    h2_answer.id = 'id-answer';
 
     div.appendChild(h2_question)
     div.appendChild(h2_answer)
 
     div.addEventListener("click", function () {
-        if (h2_answer.style.display == "none")
+        if (editMode == true)
             h2_answer.style.display = "block";
-        else
-            h2_answer.style.display = "none";
+        else {
+            if (h2_answer.style.display == "none")
+                h2_answer.style.display = "block";
+            else
+                h2_answer.style.display = "none";
+        }
     })
 
     flashcards.appendChild(div);
 }
-
 
 function addFlashcard() {
     let flashcard_info = {
@@ -49,6 +64,25 @@ function addFlashcard() {
     divMaker(contentArray[contentArray.length - 1]);
     question.value = '';
     answer.value = '';
+}
+
+function editFlashcards() {
+    editMode ? editMode = false : editMode = true;
+    console.log(editMode);
+
+    nodeBtn.forEach(el => {
+        el.childNodes.forEach(x => {
+            if (x.id == 'edit-card') {
+                x.style.display == 'none' ? x.style.display = 'block' : x.style.display = 'none'
+            }
+            if (editMode == true && x.id == 'id-answer') {
+                x.style.display = 'block'
+            }
+            if (editMode == false && x.id == 'id-answer'){
+                x.style.display = 'none'
+            }
+        });
+    })
 }
 
 function showCreateCardBox() {
