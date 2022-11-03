c2bfdedf9c35783fcb0b9cc99d0ae1088caaedbc
Kike0013 <kique0013@hotmail.com>
2022-11-02 21:55:11 -0400

    Added sample data generator

--- app.js
+++ app.js
@@ -3,45 +3,66 @@ const createBox = document.getElementsByClassName("create-box")[0];
 const question = document.getElementById("question")
 const answer = document.getElementById("answer")
 const saveEditBtn = document.getElementById("save-update")
+const closeBtn = document.getElementById("close")
 let nodeBtn = flashcards.childNodes;
 let contentArray = localStorage.getItem('fcards') ? JSON.parse(localStorage.getItem('fcards')) : [];
 let editMode = false;
 let currentId = 0;
 
-const delFlashcards = () => {
-    localStorage.clear();
-    flashcards.innerHTML = '';
-    contentArray = [];
-}
-
 contentArray.forEach(divMaker);
+
 function divMaker(text) {
     let div = document.createElement("div");
+    let editBtn = document.createElement("button");
+    let deleteBtn = document.createElement("button");
     let h2_question = document.createElement("h2");
     let h2_answer = document.createElement("h2");
 
-    //Edit card button code
-    let editBtn = document.createElement("button");
-    editBtn.innerHTML = 'Edit';
-    editBtn.id = 'edit-card'
-    editBtn.setAttribute('style', "display: none; float: left; padding: 5px; margin: 2px")
-    div.appendChild(editBtn)
+    div.id = text.id;
+    div.className = 'flashcard';
 
-    editBtn.addEventListener('click', () => {
-        question.value = h2_question.textContent
-        answer.value = h2_answer.textContent
-        currentId = div.id;
+    editBtn.id = 'edit-card'
+    editBtn.innerHTML = 'Edit';
+    editBtn.setAttribute('style', "display: none; float: left; padding: 5px; margin: 2px")
+
+    deleteBtn.id = 'delete-card'
+    deleteBtn.innerHTML = 'X';
+    deleteBtn.setAttribute('style', "display: none; float: right; padding: 5px; margin: 2px")
+
+    h2_question.id = 'id-question';
+    h2_question.innerHTML = text.my_question;
+    h2_question.setAttribute('style', "border-top: 1px solid red; padding: 15px; margin-top: 30px;")
+
+    h2_answer.id = 'id-answer';
+    h2_answer.innerHTML = text.my_answer;
+    h2_answer.setAttribute('style', "text-align: center; display: none; color: red; padding-left: 8px; padding-right: 8px;")
+
+    div.appendChild(editBtn)
+    div.appendChild(deleteBtn)
+    div.appendChild(h2_question)
+    div.appendChild(h2_answer)
+
+    //Show/hide answer
+    div.addEventListener("click", function () {
+        if (editMode == true) {
+            h2_answer.style.display = "block";
+        }
+        else {
+            if (h2_answer.style.display == "none")
+                h2_answer.style.display = "block";
+            else
+                h2_answer.style.display = "none";
+        }
     })
 
-    //^^^^^^^^^^^^^^^^^^^
+    //Edit Flashcard
+    editBtn.addEventListener('click', () => {
+        currentId = div.id;
+        question.value = h2_question.textContent
+        answer.value = h2_answer.textContent
+    })
 
-    //Delete card button code
-    let deleteBtn = document.createElement("button");
-    deleteBtn.innerHTML = 'X';
-    deleteBtn.id = 'delete-card'
-    deleteBtn.setAttribute('style', "display: none; float: right; padding: 5px; margin: 2px")
-    div.appendChild(deleteBtn)
-
+    //Delete Flashcard
     deleteBtn.addEventListener('click', () => {
         contentArray.map(el => {
             if (el.id == div.id) {
@@ -51,33 +72,6 @@ function divMaker(text) {
 
         localStorage.setItem('fcards', JSON.stringify(contentArray));
         div.remove();
-    })
-
-    //^^^^^^^^^^^^^^^^^^^
-
-    div.className = 'flashcard';
-    div.id = text.id;
-
-    h2_question.setAttribute('style', "border-top: 1px solid red; padding: 15px; margin-top: 30px;")
-    h2_question.innerHTML = text.my_question;
-    h2_question.id = 'id-question';
-
-    h2_answer.setAttribute('style', "text-align: center; display: none; color: red;")
-    h2_answer.innerHTML = text.my_answer;
-    h2_answer.id = 'id-answer';
-
-    div.appendChild(h2_question)
-    div.appendChild(h2_answer)
-
-    div.addEventListener("click", function () {
-        if (editMode == true)
-            h2_answer.style.display = "block";
-        else {
-            if (h2_answer.style.display == "none")
-                h2_answer.style.display = "block";
-            else
-                h2_answer.style.display = "none";
-        }
     })
 
     flashcards.appendChild(div);
@@ -97,22 +91,11 @@ function addFlashcard() {
         question.value = '';
         answer.value = '';
     } else {
-        console.log('Update, modo edit.');
-
         contentArray.map(el => {
             if (el.id == currentId) {
                 el.my_question = question.value;
                 el.my_answer = answer.value;
             }
-        })
-
-
-        nodeBtn.forEach(node => {
-            node.childNodes.forEach(x => {
-                if (x.parentNode.id == currentId) {
-                    console.log(x.parentNode);
-                }
-            })
         })
 
         nodeBtn.forEach(node => {
@@ -134,7 +117,6 @@ function addFlashcard() {
         answer.value = '';
 
         currentId = 0;
-
     }
 }
 
@@ -142,20 +124,19 @@ function editFlashcards() {
     showCreateCardBox();
     if (editMode) {
         editMode = false
+        closeBtn.removeAttribute("disabled")
         saveEditBtn.innerText = 'Save'
         question.value = '';
         answer.value = '';
         currentId = 0;
     } else {
         editMode = true;
+        closeBtn.setAttribute("disabled", "")
         saveEditBtn.innerText = 'Update'
         question.value = '';
         answer.value = '';
         currentId = 0;
     }
-
-    console.log(editMode);
-
 
     nodeBtn.forEach(el => {
         el.childNodes.forEach(x => {
@@ -181,4 +162,18 @@ function showCreateCardBox() {
 
 function hideCreateBox() {
     createBox.style.display = "none"
+}
+
+const delFlashcards = () => {
+    localStorage.clear();
+    flashcards.innerHTML = '';
+    contentArray = [];
+}
+
+const generateSampleData = () => {
+    fetch('./sampleData.json')
+        .then(res => res.json())
+        .then(json => {for (const elem of json) {
+            divMaker(elem);
+        }})
 }
